68fd5726751d76555a9a4df5906646dbc2c79d54
ISAO-SERVER\isao <isa0.net@ezweb.ne.jp>
2018-02-03 23:02:49 +0900

    2重起動防止コード + 起動されているアプリの表示

--- PlainWpf/SingleBootableApp.cs
+++ PlainWpf/SingleBootableApp.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Ipc;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -10,6 +14,7 @@ namespace PlainWpf
     /// <summary>
     /// 2重起動防止アプリケーションクラス
     /// http://kisuke0303.sakura.ne.jp/blog/?p=182 WPF で二重起動を防止する
+    /// http://www.geocities.jp/litorud/WPF.html#9 二重起動を防止し、起動中のウィンドウをアクティブにする
     /// 
     /// スタートアップクラスAppの基底クラスApplicationと差し替えて使う
     /// xamlの基底クラスも差し替える
@@ -36,7 +41,11 @@ namespace PlainWpf
             DoubleBootRelease();
         }
 
-        #region 2重起動防止コード
+        #region 2重起動防止コード + 起動されているアプリの表示
+        /// <summary>
+        /// 実際はUUIDなどが良い。
+        /// </summary>
+        const string applicationId = "00000000-0000-0000-0000-000000000000";
         /// <summary>
         /// コンストラクタでアプリケーション名を習得
         /// </summary>
@@ -45,7 +54,7 @@ namespace PlainWpf
         /// 起動フラグ用ミューテックス
         /// アプリケーション名使用
         /// </summary>
-        private System.Threading.Mutex mutex;
+        private static System.Threading.Mutex mutex;
         /// <summary>
         /// スタートアップ時のミューテックスのチェックと拾得
         /// </summary>
@@ -54,11 +63,22 @@ namespace PlainWpf
             // ミューテックスの所有権を要求
             if (!mutex.WaitOne(0, false))
             {
-                // 既に起動しているため終了させる
                 MessageBox.Show(appName + " は既に起動しています。", "二重起動防止", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+
+                // 起動されているアプリの表示
+                ChannelServices.RegisterChannel(new IpcClientChannel(), true);
+                ((IpcHandler)Activator.GetObject(typeof(IpcHandler), "ipc://" + applicationId + "/" + appName)).Handle();
+
+                // 既に起動しているため終了させる
                 mutex.Close();
                 mutex = null;
                 this.Shutdown();
+            }
+            else
+            {
+                // セマフォの登録
+                ChannelServices.RegisterChannel(new IpcServerChannel(applicationId), true);
+                RemotingServices.Marshal(new IpcHandler(), appName, typeof(IpcHandler));
             }
         }
         /// <summary>
@@ -71,6 +91,25 @@ namespace PlainWpf
                 mutex.ReleaseMutex();
                 mutex.Close();
             }
+        }
+
+        /// <summary>
+        /// IPC用ハンドラ
+        /// </summary>
+        private class IpcHandler : MarshalByRefObject
+        {
+            public void Handle()
+            {
+                Current.Dispatcher.Invoke(() => {
+                    Current.MainWindow.Activate();
+                    if (Current.MainWindow.WindowState == WindowState.Minimized)
+                    {
+                        Current.MainWindow.WindowState = WindowState.Normal;
+                    }
+                });
+            }
+
+            public override object InitializeLifetimeService() => null;
         }
         #endregion
     }

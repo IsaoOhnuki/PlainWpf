8480d6bbd558ea7a842e9473d242b170136aee3f
ISAO-SERVER\isao <isa0.net@ezweb.ne.jp>
2018-02-04 01:21:29 +0900

    MvvmHelper追加

---
+++ PlainWpf/MvvmHelper/BindableBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmHelper
+{
+    abstract public class BindableBase : INotifyPropertyChanged
+    {
+        protected BindableBase() { }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] String propertyName = null)
+        {
+            if (object.Equals(storage, value))
+                return false;
+            storage = value;
+            this.RaisePropertyChanged(propertyName);
+            return true;
+        }
+    }
+}

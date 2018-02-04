200f1808b31ce735ddd297155d72b56acb8d4570
ISAO-SERVER\isao <isa0.net@ezweb.ne.jp>
2018-02-04 18:03:21 +0900

    レイアウトを切り替えるページを作った

--- PlainWpf/ViewModels/SabPage2ViewModel.cs
+++ PlainWpf/ViewModels/SabPage2ViewModel.cs
@@ -1,12 +1,65 @@
-﻿using System;
+﻿using MvvmHelper;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace PlainWpf.ViewModels
 {
-    public class SabPage2ViewModel
+    public class SabPage2ViewModel : BindableBase
     {
+        private Orientation orientation;
+        public Orientation Orientation
+        {
+            get { return orientation; }
+            set { SetProperty(ref orientation, value); }
+        }
+
+        public DelegateCommand OrientationChangeCommand { get; set; }
+
+        public SabPage2ViewModel()
+        {
+            OrientationChangeCommand = new DelegateCommand(() => {
+                if (Orientation == Orientation.Horizontal)
+                {
+                    Orientation = Orientation.Vertical;
+                }
+                else
+                {
+                    Orientation = Orientation.Horizontal;
+                }
+            });
+        }
+    }
+
+    public class OrientationHorizontalToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Orientation)value) == Orientation.Horizontal ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Visibility)value) == Visibility.Visible ? Orientation.Horizontal : Orientation.Vertical;
+        }
+    }
+
+    public class OrientationVerticalToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Orientation)value) == Orientation.Vertical ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Visibility)value) == Visibility.Visible ? Orientation.Vertical : Orientation.Horizontal;
+        }
     }
 }
